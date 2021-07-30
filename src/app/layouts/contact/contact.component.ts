d72cb3191dfdd1d98c16a16f5669c53d11d69ef0
murty.av <murty@vibhumit.com>
2021-07-30 18:00:52 +0530

    EmailJS added to contact page

--- src/app/layouts/contact/contact.component.ts
+++ src/app/layouts/contact/contact.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import emailjs, { EmailJSResponseStatus } from 'emailjs-com';
+import Swal from 'sweetalert2'
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.css']
+	selector: 'app-contact',
+	templateUrl: './contact.component.html',
+	styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+	constructor() { }
 
+	public sendEmail(e: Event) {
+		e.preventDefault();
+		emailjs.sendForm('service_0x57b1m', 'template_8yj27fr', e.target as HTMLFormElement, 'user_cE6wxEo2qBLDryxDA6XIL')
+			.then((result: EmailJSResponseStatus) => {
+				console.log(result.text);
+				Swal.fire(
+					'Email Sent!',
+					'Will get back to you shortly!',
+					'success'
+				);
+				(e.target as HTMLFormElement).reset();
+			}, (error) => {
+				console.log(error.text);
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops...',
+					text: 'Something went wrong!',
+				})
+			});
+	}
+
+	ngOnInit(): void {
+	}
 }
